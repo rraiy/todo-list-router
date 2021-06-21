bd409f50b1a8fec8af43716b81573f3a1f50f40e
rraiy <raiybow@gmail.com>
2021-06-21 23:04:39 +0800

    basic

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const App = () => {
+
+    return(
+        <div>App.js</div>
+    )   
+}
+
+export default App;
